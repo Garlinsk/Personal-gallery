9579bfcfd68e9083c5deacee4e08007bbbb8c983
franklin <franklinwanami@gmail.com>
2021-09-05 09:20:47 +0300

    update templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my gallery')
+
+
+def index(request):
+    date = dt.date.today()
+    # images = Image.get_images()
+    # location = Location.get_location()
+    # locations = Location.get_location()
+
+    return render(request, 'index.html', {"date": date})
