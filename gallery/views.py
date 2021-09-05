1e24a03bad702bd6eed1a539b14068f4a898f69c
franklin <franklinwanami@gmail.com>
2021-09-05 23:05:45 +0300

    added templates

--- gallery/views.py
+++ gallery/views.py
@@ -3,7 +3,8 @@ from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-
+def welcome(request):
+    return HttpResponse('Welcome to my gallery')
 
 def index(request):
     date = dt.date.today()
