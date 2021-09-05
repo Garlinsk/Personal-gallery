75209844854deedbd03e24e60e9e47f302fd29d7
franklin <franklinwanami@gmail.com>
2021-09-06 00:39:44 +0300

    modified files

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$', views.index, name= 'index'),
+    # url(r'^search/', views.search_images, name='search_results'),
+    # url(r'^image/(\d+)', views.get_image, name='image_results'),
+    # url(r'^location/(?P<location>\w{0,50})/', views.location, name='location_results'),
+    # url(r'^category/(?P<category>\w{0,50})/', views.category, name='category_results'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -3,8 +3,6 @@ from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my gallery')
 
 def index(request):
     date = dt.date.today()
