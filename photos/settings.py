ace8e9892c4ee0c618845f73ad35c9f98ac357ce
franklin <franklinwanami@gmail.com>
2021-09-06 22:39:31 +0300

    updated my files

--- photos/settings.py
+++ photos/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5',
+    'bootstrap4',
     'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
